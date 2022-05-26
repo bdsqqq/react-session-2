98f5940575a2ad1b6abe64852b23c3042b5c529b
Igor Bedesqui <igorbedesqui@gmail.com>
2022-05-26 17:33:00 -0300

    feat: add Searchbar to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
 const Home: NextPage = () => {
   return (
-    <div>
-      Hej do <code>0</code>
-    </div>
+    <Box
+      css={{
+        maxWidth: "1145px",
+        margin: "0 auto",
+      }}
+    >
+      <Box as="main">
+        <Box
+          css={{
+            maxWidth: "715px",
+            margin: "$spacing-07 auto 0",
+            padding: "0 $spacing-05",
+
+            "@md": {
+              margin: "$spacing-10 auto 0",
+              padding: "0",
+            },
+          }}
+        >
+          <SearchBar />
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
 export default Home;
 
 import type { NextPage } from "next";
+
+import SearchBar from "@/components/ui/SearchBar";
+import { Box } from "@/components/ui/primitives";
